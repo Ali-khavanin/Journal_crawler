c45db0ca077b3debfc12aae33597fc28d0f86dae
Alikh <khavanin.a2015@gmail.com>
2020-02-08 11:28:24 +0330

    a automated web driver -drop down handling -its vital to make the windows max size cause the content of the page !!!!!

--- sel.py
+++ sel.py
@@ -1,5 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome(executable_path="./chromedriver.exe")
-driver.get("http://www.google.com")
+driver.get('http://jha.iums.ac.ir/')
+# driver.implicitly_wait(20)
+driver.maximize_window()
+# driver.find_element_by_link_text('آرشیو مجله و مقالات').click()
+menu = driver.find_element_by_link_text('آرشیو مجله و مقالات')
+ActionChains(driver).move_to_element(menu).perform()
+driver.find_element_by_link_text('کلیه شماره‌های مجله').click()
+
+
 
