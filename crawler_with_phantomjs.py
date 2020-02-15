33eb77b617bfaa56accb0932317f992221073cf8
Alikh <khavanin.a2015@gmail.com>
2020-02-15 16:14:08 +0330

    some changes

--- crawler_with_phantomjs.py
+++ crawler_with_phantomjs.py
@@ -18,16 +18,27 @@ i = 1
 for plus in driver.find_elements_by_xpath(
         "//a[contains(@onclick, 'loadIssues')]"):  # browser.find_elements_by_class_name("dv_archive_vol"):
     try:
-        element = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[contains(@onclick, 'loadIssues')]")))
-        print("link number  ", i, "is found ")
-        # print(plus)
-        plus.click()
-        print("plus number ", i, " is clicked   ", str(plus.click()))
-        time.sleep(10)  # plus.find_element_by_tag_name("a").click()
-        i += 1
+        if i == 1:
+            print("link number  ", i, "is found ")
+            print("plus number ", i, " is NOT clicked cause it is a minus !  ")
+            i += 1
+            time.sleep(5)
+        else:
+            print(' i = ', i)
+            element = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, "//a[contains(@onclick, 'loadIssues')]")))
+            print("link number  ", i, "is found ")
+            # print(plus)
+            plus.click()
+            print("plus number ", i, " is clicked   ", str(plus.click()))
+            time.sleep(5)  # plus.find_element_by_tag_name("a").click()
+            i += 1
     except Exception as exc:
         print("something went wrong ! in link number : ", i)
         print(exc)
         i += 1
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+out_source = open('out.txt', 'w')
+print(soup)
 # driver.implicitly_wait(time_to_wait=5)
