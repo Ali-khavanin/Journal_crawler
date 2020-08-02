f7e5f10717c16a2ed8139ec711dab84d1e07d40a
Alikh <khavanin.a2015@gmail.com>
2020-08-02 11:41:21 +0430

    code is modified to store all urls in a text file tbn : its a good idea to use pickl files for efficiency

--- getAllYektaWebs.py
+++ getAllYektaWebs.py
@@ -5,10 +5,9 @@ yektaWeb_dataBase_url = "https://yektaweb.com/find.php?item=1.161.248.fa"
 
 response = get(yektaWeb_dataBase_url)
 # print(response.text)
-
+file = open('allYektaWebs.txt', 'w', encoding='utf-8')
 soup = BeautifulSoup(response.text, 'html.parser')
 
-for i in soup.find_all('a',target='_blank'):
-    print(i.get('href'))
-    print('\n')
-
+for i in soup.find_all('a', target='_blank'):
+    file.write(i.get('href') + '\n')
+file.close()
