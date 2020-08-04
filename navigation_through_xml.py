b198e8fb96c0f0651796f3bc6283cd13dddf93c2
Alikh <khavanin.a2015@gmail.com>
2020-08-04 15:41:44 +0430

    this code gets the volume number as well as the link to download it !

--- navigation_through_xml.py
+++ navigation_through_xml.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+
 tree = ET.parse('1.xml')
 root = tree.getroot()
 
@@ -6,9 +7,13 @@ print(root.tag)
 
 print(root.attrib)
 
-for child in root:
-    print(child.tag)
-    for link in child.iter('ArchiveCopySource'):
-        print(link.text)
+for Article in root.findall('.//Article'):
+    print(Article.find("ArchiveCopySource").text+ "----" + Article.find('Journal/Volume').text)
+
 # gets all the download links
+
+for volume in root:
+    for v in volume.iter('Journal'):
+        print(v.attrib)
+
 
