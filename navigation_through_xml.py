1686494e440d21fe5942b9f3f70687702440353d
Alikh <khavanin.a2015@gmail.com>
2020-08-09 15:46:53 +0430

    now it navigates through multiple xml files for further usage
    (another feature will be add that it will delete the xml files after extracting the articles !)
    
    (after that it is suppose to make a pickle file from article set)

--- navigation_through_xml.py
+++ navigation_through_xml.py
@@ -3,41 +3,47 @@ from Article_class import Article
 from Article_class import Author
 
 
+# from crawler_with_phantomjs import issue_number
+
 def get_Authors(path):
     Authors = []
     for author in path.findall('./AuthorList//Author'):
-        Authors.append(Author(author.find('FirstName').text, author.find('LastName').text , author.find('Affiliation').text))
+        Authors.append(
+            Author(author.find('FirstName').text, author.find('LastName').text, author.find('Affiliation').text))
     return Authors
 
 
 articles: Article = []
 
-xml_file_adress = '1.xml'
-tree = ET.parse(xml_file_adress)
-root = tree.getroot()
-
-print(root.tag)
-
-print(root.attrib)
 number: int = 1
+for xml_number in range(1, 49):
+    xml_file_adress = './' + str(xml_number) + '.xml'
+    tree = ET.parse(xml_file_adress)
+    root = tree.getroot()
 
-for element in root.findall('.//Article'):
-    link_to_download = element.find("ArchiveCopySource").text
-    volume = element.find('Journal/Volume').text
-    issue = element.find('Journal/Issue').text
-    abstract = element.find('OtherAbstract').text
-    title = element.find('ArticleTitle').text
-    year = element.find('Journal/PubDate/Year').text
-    for author in element.findall('./AuthorList//Author'):
-        print(author.find('FirstName').text + '   '
-              + author.find('LastName').text)
-    print(abstract)
-    print(link_to_download + "----" + volume.zfill(3)
-          + '---------' + issue.zfill(3))
+    last_issue = 0
+    for element in root.findall('.//Article'):
+        link_to_download = element.find("ArchiveCopySource").text
+        volume = element.find('Journal/Volume').text
+        issue = element.find('Journal/Issue').text
+        abstract = element.find('OtherAbstract').text
+        title = element.find('ArticleTitle').text
+        year = element.find('Journal/PubDate/Year').text
+        if last_issue != issue:
+            number = 1
+            last_issue = issue
+        else:
+            number += 1
+        for author in element.findall('./AuthorList//Author'):
+            print(author.find('FirstName').text + '   '
+                  + author.find('LastName').text)
+        print(abstract)
+        print(link_to_download + "----" + volume.zfill(3)
+              + '---------' + issue.zfill(3))
 
-    articles.append(Article(link_to_download, volume, issue, str(number), get_Authors(element) , abstract
-                            , title,year))
-    number += 1
+        articles.append(Article(link_to_download, volume, issue, str(number), get_Authors(element), abstract
+                                , title, year))
+
 # print(articles[0].link_to_download)
 # gets all the download links and volumes
 # print(articles[0].Authors[0].FirstName)
