df68341aca4eef538764cbdff87c19c96f151ccc
Alikh <khavanin.a2015@gmail.com>
2020-08-09 16:05:57 +0430

    exception handling added ! (simple version)

--- download_and_save.py
+++ download_and_save.py
@@ -4,15 +4,21 @@ from navigation_through_xml import articles
 
 
 def download_to(url: str, path: str):
-    with open(path, 'wb') as pdfFile:
-        pdfFile.write(requests.get(url).content)
+    try:
+        with open(path, 'wb') as pdfFile:
+            pdfFile.write(requests.get(url).content)
+    except Exception as exp:
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("some thing went wrong - >", exp)
+        print("it was in link ", url)
+        print("the path is ->", path)
 
 
 if not os.path.isdir('./0'):
     os.mkdir('./0')
 pth = './0/'
 for article in articles:
-    nameOfTheFile = '0' + article.JournalCode + article.volume+ article.issue + article.number + '.pdf'
+    nameOfTheFile = '0' + article.JournalCode + article.volume + article.issue + article.number + '.pdf'
     pth = './0/'
     if not os.path.isdir(pth + article.JournalCode):
         os.mkdir(pth + article.JournalCode)
