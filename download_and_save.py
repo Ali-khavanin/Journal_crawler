59b1e31a1321a4c707a7afcb74bdfe1deb3ff3ed
Alikh <khavanin.a2015@gmail.com>
2020-08-10 10:27:52 +0430

    a minor sleep added so that the program doesnt get baned by the site

--- download_and_save.py
+++ download_and_save.py
@@ -1,9 +1,11 @@
 import requests
 import os
 from navigation_through_xml import articles
+import time
 
 
 def download_to(url: str, path: str):
+    time.sleep(10)
     try:
         with open(path, 'wb') as pdfFile:
             pdfFile.write(requests.get(url).content)
@@ -12,13 +14,15 @@ def download_to(url: str, path: str):
         print("some thing went wrong - >", exp)
         print("it was in link ", url)
         print("the path is ->", path)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 
 if not os.path.isdir('./0'):
     os.mkdir('./0')
 pth = './0/'
 for article in articles:
-    nameOfTheFile = '0' + article.JournalCode + article.volume + article.issue + article.number + '.pdf'
+
+    nameOfTheFile = '0' + article.JournalCode.zfill(8) + article.volume.zfill(4) + article.issue.zfill(3) + article.number.zfill(2) + '.pdf'
     pth = './0/'
     if not os.path.isdir(pth + article.JournalCode):
         os.mkdir(pth + article.JournalCode)
@@ -31,7 +35,7 @@ for article in articles:
     pth = pth + article.volume + '/'
     # pth = pth + article.number + '.pdf'
     print("the name of the file weill be ", nameOfTheFile)
-
+    time.sleep(10)
     download_to(article.link_to_download, pth + nameOfTheFile)
     print("file ", nameOfTheFile, " from ", article.link_to_download, " is downloaded !")
     print("********************************************************************************")
