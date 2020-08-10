df7814c74a32e834592ebb33bef902619404d2dd
Alikh <khavanin.a2015@gmail.com>
2020-08-10 11:21:04 +0430

    it is now capsulized and can be used as a function new funtion has been added to delete the xml files after extraction

--- navigation_through_xml.py
+++ navigation_through_xml.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 from Article_class import Article
 from Article_class import Author
+from save_load_pickle import save_obj
+import os
 
 
 # from crawler_with_phantomjs import issue_number
@@ -15,38 +17,43 @@ def get_Authors(path):
 
 articles: Article = []
 
-number: int = 1
-for xml_number in range(1, 49):
-    xml_file_adress = './' + str(xml_number) + '.xml'
-    tree = ET.parse(xml_file_adress)
-    root = tree.getroot()
 
-    last_issue = 0
-    for element in root.findall('.//Article'):
-        link_to_download = element.find("ArchiveCopySource").text
-        volume = element.find('Journal/Volume').text
-        issue = element.find('Journal/Issue').text
-        abstract = element.find('OtherAbstract').text
-        title = element.find('ArticleTitle').text
-        year = element.find('Journal/PubDate/Year').text
-        if last_issue != issue:
-            number = 1
-            last_issue = issue
-        else:
-            number += 1
-        for author in element.findall('./AuthorList//Author'):
-            print(author.find('FirstName').text + '   '
-                  + author.find('LastName').text)
-        print(abstract)
-        print(link_to_download + "----" + volume.zfill(3)
-              + '---------' + issue.zfill(3))
+def creat_articles(count_xmls: int, journalCode):
+    number: int = 1
+    for xml_number in range(1, count_xmls + 1):
+        xml_file_adress = './' + str(xml_number) + '.xml'
+        tree = ET.parse(xml_file_adress)
+        root = tree.getroot()
 
-        articles.append(Article(link_to_download, volume, issue, str(number), get_Authors(element), abstract
-                                , title, year))
+        last_issue = 0
+        for element in root.findall('.//Article'):
+            link_to_download = element.find("ArchiveCopySource").text
+            volume = element.find('Journal/Volume').text
+            issue = element.find('Journal/Issue').text
+            abstract = element.find('OtherAbstract').text
+            title = element.find('ArticleTitle').text
+            year = element.find('Journal/PubDate/Year').text
+            # journalName = element.find
+            if last_issue != issue:
+                number = 1
+                last_issue = issue
+            else:
+                number += 1
+            for author in element.findall('./AuthorList//Author'):
+                print(author.find('FirstName').text + '   '
+                      + author.find('LastName').text)
+            print(abstract)
+            print(link_to_download + "----" + volume.zfill(3)
+                  + '---------' + issue.zfill(3))
 
-# print(articles[0].link_to_download)
-# gets all the download links and volumes
-# print(articles[0].Authors[0].FirstName)
-# print(articles[4].title)
-# print(articles[0].year)
-# print(articles[2].number)
+            articles.append(
+                Article(link_to_download, journalCode, volume, issue, str(number), get_Authors(element), abstract
+                        , title, year))
+
+    def save_articles_list():
+        save_obj(articles, './articles.pkl')
+
+    def delete_xmls(count: int):
+        for i in range(1, count + 1):
+            os.remove('./' + i + '.xml')
+        print("xml files are deleted !")
