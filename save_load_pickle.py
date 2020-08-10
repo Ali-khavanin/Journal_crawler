a748438eeb4666fd4c860d998e2d7fdd7a3f44b0
Alikh <khavanin.a2015@gmail.com>
2020-08-10 10:59:44 +0430

    it saves and loads pickles

---
+++ save_load_pickle.py
@@ -0,0 +1,11 @@
+import pickle
+
+
+def save_obj(obj, path):
+    with open(path , 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(path):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
