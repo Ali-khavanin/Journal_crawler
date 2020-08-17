6fe71ccb2129254a42c57279d0e75ae6cc940aa2
Alikh <khavanin.a2015@gmail.com>
2020-08-17 11:50:28 +0430

    it detects the language of a web page to avoid downloading ENG articles

---
+++ detect_webpage_language.py
@@ -0,0 +1,17 @@
+from langdetect import detect
+
+from selenium import webdriver
+
+
+def get_webpage_lang(url):
+    driver = webdriver.PhantomJS(
+        executable_path="C:/Users/khavaninzadeh/Desktop/phantomjs-2.1.1-windows/bin/phantomjs.exe")
+
+    # url = "http://jh-per.halal.ac.ir/"
+    driver.get(url)
+    el = driver.find_element_by_tag_name('body')
+    # print(el.text)
+    print("the lang of this page is : ", detect(el.text))
+    if detect(el.text == 'fa'):
+        return True
+    return False
